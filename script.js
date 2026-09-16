9ddb853e253bc7fc195bcaecae4cc1fa2b774a76
albuquerquesilvaalexsandra-sketch <albuquerque.silva.alexsandra@escola.pr.gov.br>
2026-09-16 17:55:13 -0300

    atualizei meu codigo

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+const botoescurtir =document.queryselectorAll(".curtir");
+     botoescurtir. forEach(function(botaoCutir){
+     let curtiu =false;
+      botaoCurtir.addEventlistener("click", curtir);
+      function curtir(){
+ const contador =botaoCurtir.queryselector("span");
+ if(curtiu === false){
+  contador. textcontent++;
+ curtiu = true;}
+     else{
+     contador.textcontent--;
+     curtiu = false; 
+     }
+}
+}
